760ccbb3b4c351742f04976182db3ba3bd8f0c02
Ali Alizade Haqiqi <alializade@peedes.com>
2019-01-15 16:40:39 +0330

    Formula appended.

---
+++ vajehyab.rb
@@ -0,0 +1,13 @@
+class Vajehyab < Formula
+  desc "English, Persian, and Arabic dictionary"
+  homepage "https://www.vajehyab.com/"
+  version "0.1.0"
+  url "https://github.com/alializade/vajehyab/archive/v0.1.0.tar.gz"
+  sha256 "591c0e17064bda2f065395a290527e5789d363d2a19df47ecbb8a49d79fa07d4"
+
+  depends_on "jq"
+
+  def install
+    bin.install "vajehyab"
+  end
+end
