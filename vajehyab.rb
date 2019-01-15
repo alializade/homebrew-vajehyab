04fe7845eae89d9bee499b382fd7c676fcd6b8bf
Ali Alizade Haqiqi <alializade@peedes.com>
2019-01-15 17:27:13 +0330

    Some isssues have been fixed.

--- vajehyab.rb
+++ vajehyab.rb
@@ -1,13 +1,13 @@
 class Vajehyab < Formula
   desc "English, Persian, and Arabic dictionary"
   homepage "https://www.vajehyab.com/"
-  version "0.1.0"
   url "https://github.com/alializade/vajehyab/archive/v0.1.0.tar.gz"
   sha256 "591c0e17064bda2f065395a290527e5789d363d2a19df47ecbb8a49d79fa07d4"
 
-  depends_on "jq"
+  depends_on "bash" => :run
+  depends_on "jq" => :run
 
   def install
-    bin.install "vajehyab"
+    bin.install "bin/vajehyab"
   end
 end
